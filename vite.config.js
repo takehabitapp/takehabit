1364b5eff559ac01b58ca53633c9f4c61731762d
Dario Balado <takehabit.app@gmail.com>
2026-03-19 11:35:52 +0100

    feat: implement automated sitemap generation with vite-plugin-sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Sitemap({ 
+      hostname: 'https://takehabit.com',
+      dynamicRoutes: [
+        '/',
+        '/onboarding',
+        '/login',
+        '/register',
+        '/dashboard',
+        '/create-habit',
+        '/habits',
+        '/privacy-policy',
+        '/terms-and-conditions',
+      ]
+    })
+  ],
 })
